ae890debce71896f1cbe76fed3857580292ae6eb
Alex <74885892+misspoken69@users.noreply.github.com>
2021-04-06 14:28:58 -0700

    Add logs

--- index.js
+++ index.js
@@ -8,3 +8,17 @@ const prefix = 'm!';
 const keepAlive = require('./server.js');
 
 // Made by Misspoken#1122. Don't skid this shit. :)
+
+antinuke.on("ready", async () => {
+    console.clear();
+  console.log("███╗░░░███╗██╗░██████╗███████╗██████╗░██╗░░░██╗")
+  console.log("████╗░████║██║██╔════╝██╔════╝██╔══██╗╚██╗░██╔╝")
+  console.log("██╔████╔██║██║╚█████╗░█████╗░░██████╔╝░╚████╔╝░")
+  console.log("██║╚██╔╝██║██║░╚═══██╗██╔══╝░░██╔══██╗░░╚██╔╝░░")
+  console.log("██║░╚═╝░██║██║██████╔╝███████╗██║░░██║░░░██║░░░")
+  console.log("╚═╝░░░░░╚═╝╚═╝╚═════╝░╚══════╝╚═╝░░╚═╝░░░╚═╝░░░")
+  console.log("———————————————————————————————————————————————")
+  console.log("[-] Thanks for using Misery Anti-Nuke!")
+  console.log("[-] If you like it, maybe leave a star and follow?")
+  console.log("[-] It would help a lot considering I am making more!")
+  console.log("[-] This was made by Misspoken so don't skid this shit.")
