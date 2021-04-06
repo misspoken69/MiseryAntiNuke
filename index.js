3f8ce25b5b18a78cb25052a1b945b3b338974194
Alex <74885892+misspoken69@users.noreply.github.com>
2021-04-06 14:26:49 -0700

    Update index.js

--- index.js
+++ index.js
@@ -6,3 +6,5 @@ const clc = require('cli-color');
 const config = require('./config.json'); 
 const prefix = 'm!';
 const keepAlive = require('./server.js');
+
+// Made by Misspoken#1122. Don't skid this shit. :)
